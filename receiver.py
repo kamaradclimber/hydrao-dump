1619491a0f4288fa2abad84ecfb48c93703a43d6
Grégoire Seux <kamaradclimber@gmail.com>
2021-10-03 20:09:01 +0200

    Improve interaction with HA mqtt discovery
    Change-Id: Ic1c59dd90b5c44884133142ec8d638444eb62de6

--- receiver.py
+++ receiver.py
@@ -8,7 +8,6 @@ import paho.mqtt.client as mqtt
 
 # Credits: https://itnext.io/ble-and-gatt-for-iot-2ae658baafd5
 
-
 def print_peripheral(peripheral):
     print("Discovering Services...")
     services = peripheral.getServices()  # a first discovery is necessary apparently
@@ -24,9 +23,10 @@ def main():
     mqtt_client = build_mqtt_client()
     while True:
         try:
+            print("connect and read")
             connect_and_read(mqtt_client)
         except btle.BTLEDisconnectError as e:
-            print("Got disconnected, will retry")
+            print("Got disconnected, will retry", flush=True)
             time.sleep(1)
 
 def build_mqtt_client():
@@ -43,34 +43,37 @@ def build_mqtt_client():
     client.loop_start()
     return client
 
-def mqtt_declare_hydrao_sensors(mqtt_client, hydrao):
+def mqtt_declare_hydrao_sensors(mqtt_client, hydrao, delete_first=False):
     """
     Declare hydrao sensors in home assistant.
     According to documentation, one can declare multiple sensors sharing the same
     state_topic to allow to update all sensors in one message.
     For now, topics are completely hardcoded
     """
-    topic = 'homeassistant/sensor/hydrao1_current_shower_volume/config'
-    config = {"name": "Current shower volume", "state_topic": "homeassistant/sensor/hydrao1/state", "unit_of_measurement": "L", "value_template": "{{ value_json.volume.current}}" }
-    mqtt_client.publish(topic, json.dumps(config))
-    topic = 'homeassistant/sensor/hydrao1_total_volume/config'
-    config = {"name": "Last 400 showers volume", "state_topic": "homeassistant/sensor/hydrao1/state", "unit_of_measurement": "L", "value_template": "{{ value_json.volume.last_400_showers}}" }
-    mqtt_client.publish(topic, json.dumps(config))
-    topic = 'homeassistant/sensor/hydrao1_water_temperature/config'
-    config = {"device_class": "temperature", "name": "Water temperature", "state_topic": "homeassistant/sensor/hydrao1/state", "unit_of_measurement": "°C", "value_template": "{{ value_json.temperature}}" }
-    mqtt_client.publish(topic, json.dumps(config))
-    
+    prefix_topic = f"homeassistant/sensor/hydrao_{hydrao.addr}"
+    config_topic = f"{prefix_topic}/config"
+    attributes_topic = f"{prefix_topic}/attributes"
+    state_topic = f"{prefix_topic}/state"
+    config = {"name": f"Hydrao shower head {hydrao.addr}", "state_class": "total_increasing", "device_class": "gas", "state_topic": state_topic, "unit_of_measurement": "L", "unique_id": hydrao.addr, "json_attributes_topic": attributes_topic }
 
-def mqtt_update_hydrao_sensors(mqtt_client, current_volume, total_volume):
-    topic = 'homeassistant/sensor/hydrao1/state'
-    state = { "temperature": 42.42, "volume": { "current": current_volume, "last_400_showers": total_volume } }
-    mqtt_client.publish(topic, json.dumps(state))
+    if delete_first:
+        mqtt_client.publish(config_topic, '')
+
+    mqtt_client.publish(config_topic, json.dumps(config))
+
+def mqtt_update_hydrao_sensors(mqtt_client, current_volume, total_volume, hydrao):
+    prefix_topic = f"homeassistant/sensor/hydrao_{hydrao.addr}"
+    attributes_topic = f"{prefix_topic}/attributes"
+    state_topic = f"{prefix_topic}/state"
+    mqtt_client.publish(state_topic, current_volume)
+    attributes = { "temperature": 42.42, "current_volume": current_volume, "last_400_showers": total_volume }
+    mqtt_client.publish(attributes_topic, json.dumps(attributes))
 
 def connect_and_read(mqtt_client=None):
     # get args
     args = get_args()
 
-    print("Connecting...")
+    print("Connecting...", flush=True)
     hydrao = btle.Peripheral(args.mac_address)
     print("Connected")
     # print_peripheral(hydrao)
@@ -88,12 +91,12 @@ def connect_and_read(mqtt_client=None):
     while True:
         now = time.asctime(time.gmtime())
         total_volume, current_volume = get_volumes(battery_service.getCharacteristics(current_volumes)[0].read())
-        print(f"{now} current_volume: {current_volume}L, total volume: {total_volume}L")
+        print(f"{now} current_volume: {current_volume}L, total volume: {total_volume}L", flush=True)
         # ca32_value_string = battery_service.getCharacteristics(ca32)[0].read()
         # print(f"{now} ca32: {ca32_value_string}")
         # print_unknown(ca32_value_string)
         if mqtt_client is not None:
-            mqtt_update_hydrao_sensors(mqtt_client, current_volume, total_volume)
+            mqtt_update_hydrao_sensors(mqtt_client, current_volume, total_volume, hydrao)
         time.sleep(2)
 
 def print_unknown(string):
